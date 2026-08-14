ca1c1dba0f2e3935d9326ae35e1c586018e5e1ee
Muowi-Nguyeen <muoinguyenquang1900@gmail.com>
2026-08-15 00:49:22 +0700

    Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://pqmedia.github.io",
+
+  integrations: [
+    sitemap(),
+  ],
 });
